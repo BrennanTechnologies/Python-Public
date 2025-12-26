bafa708a8984ecb6cc1f49831c18169a389b003b
Chris.Brennan <brennanc@hotmail.com>
2025-12-26 15:01:32 -0500

    Chris Brennan, chris@brennantechnologies.com 12/26/2025 15:01:31

--- Clean Up/HelloWorld.py
+++
@@ -1,5 +0,0 @@
-print("Hello world")
-
-x = 3
-x = x * x
-print (x)
--- junk.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-#subprocess.run(["ls", "-l"]) 
-
-#result = subprocess.run(["powershell", "pwd"], shell=True, capture_output=True, text=True)
-#print(result.stdout)
-
-import os
-os.system('echo "Geeks 4 Geeks"')
